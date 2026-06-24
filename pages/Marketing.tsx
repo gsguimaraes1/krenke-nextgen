b04f570fffaee89fa1710d78608e3407b4794823
Gabriel Guimaraes <gabriel.gbr.fire@gmail.com>
2026-06-23 23:25:44 -0300

    fix: update PowerBI embed URL with pageName param

--- pages/Marketing.tsx
+++ pages/Marketing.tsx
@@ -7,7 +7,7 @@ const MarketingPage: React.FC = () => {
         title="Relatório Marketing"
         width="100%"
         height="100%"
-        src="https://app.powerbi.com/view?r=eyJrIjoiZjUwM2ViZjEtY2I3Zi00MDYzLThiYTctOGM4OTI5OGFkMzBhIiwidCI6ImU1ZjY5ZGFiLTdkYWYtNGU1MS04MjdhLTEwNjkxOWE4ZjU5MCJ9&embedImagePlaceholder=true"
+        src="https://app.powerbi.com/view?r=eyJrIjoiZjUwM2ViZjEtY2I3Zi00MDYzLThiYTctOGM4OTI5OGFkMzBhIiwidCI6ImU1ZjY5ZGFiLTdkYWYtNGU1MS04MjdhLTEwNjkxOWE4ZjU5MCJ9&pageName=45032dae18a007e67b61"
         frameBorder="0"
         allowFullScreen
         style={{ display: 'block', border: 'none' }}
